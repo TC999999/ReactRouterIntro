16a9ac1bbee02c6cc1792406762cef5cdc572819
Tyler <insertcleverusernamehere999@gmail.com>
2024-04-19 11:18:22 -0400

    deleted unnecessary comment

--- src/Sardines.jsx
+++ src/Sardines.jsx
@@ -4,7 +4,6 @@ import "./Sardines.css";
 const Sardines = () => {
   return (
     <div className="sardines">
-      {/* <img src="https://i.gifer.com/9Ieq.gif" alt="" /> */}
       <div className="go-back center">
         <h2>YOU DON'T EAT THE SARDINES. THE SARDINES EAT YOU!</h2>
         <Link to="/">GO BACK</Link>
